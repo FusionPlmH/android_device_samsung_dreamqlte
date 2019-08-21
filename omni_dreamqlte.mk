b8a98eab366b685dbb30341ad628b5fd717da851
LeeMenHin <1249014784@qq.com>
2019-08-22 02:51:35 +0800

    Init device tree for Dreamqlte

---
+++ omni_dreamqlte.mk
@@ -0,0 +1,17 @@
+# Release name
+PRODUCT_RELEASE_NAME := dreamqlte
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk) 
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := omni_dreamqlte
+PRODUCT_DEVICE := dreamqlte
+PRODUCT_NAME := omni_dreamqlte
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-G9500
+PRODUCT_MANUFACTURER := samsung
+
