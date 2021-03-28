b02f893681d0b94401ffb91da75d4f714e97c893
Fusion <leemenhin@gmail.com>
2021-03-28 18:27:23 +0800

    Add files via upload

--- omni_dreamqlte.mk
+++ omni_dreamqlte.mk
@@ -1,16 +1,8 @@
-# Release name
-PRODUCT_RELEASE_NAME := dreamqlte
 
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk) 
 
-# Inherit some common Lineage stuff.
-# $(call inherit-product, vendor/omni/config/common.mk)
 
-
-PRODUCT_PACKAGES += \
-	charger_res_images \
-	charger
 
 PRODUCT_PACKAGES += \
 	init.recovery.usb.rc \
@@ -19,8 +11,9 @@ PRODUCT_PACKAGES += \
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := omni_dreamqlte
 PRODUCT_DEVICE := dreamqlte
-PRODUCT_NAME := omni_dreamqlte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-G9500
 PRODUCT_MANUFACTURER := samsung
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+PRODUCT_RELEASE_NAME := dreamqlte
 
